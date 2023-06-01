d39849b15d4aa41c0cc0fe7439337070fe51372a
dudasdominik <dudasdominik3@gmail.com>
2023-06-02 01:44:52 +0200

    major changes in everything

--- Ctrl-Love/src/components/Swipe.tsx
+++ Ctrl-Love/src/components/Swipe.tsx
@@ -1,32 +1,73 @@
 import logo from "../assets/logo.svg";
 import { useState, useEffect } from "react";
+import SwipeCard from "./SwipeCard";
+import "./Swipe.css"
+
+interface SwipeCardProp {
+  name: string;
+  birthDate: string;
+  photos: string[];
+  gender: string;
+  biography: string;
+  interests: string[];
+  showNext: () => void;
+}
+
 
 const Swipe = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [userIndex, setUserIndex] = useState(0);
+
+
+  const fetchData = () => {
+  fetch('http://localhost:5091/api/users')
+    .then(response => response.json())
+    .then(data => {
+      const as : SwipeCardProp[] = data;
+      setUsers(as)
+      console.log(as)
+      setIsLoading(false)
+    })
+    .catch(error => {
+    // Handle any errors
+  });
+  };
+
+  function showNext(){
+    setUserIndex((userIndex + 1 ) % users.length);
+  }
+
   useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch("http://localhost:5091/api/users", {mode:"no-cors"});
-        //const json = await response.json();
-        console.log(response)
-        //console.log(json)
-      } catch (error) {
-        console.error(error);
-      }
-    }) ();
+    fetchData();
   }, []);
 
   return (
     <>
-      <div className="swipe-wrapper">
-        <div className="swipe-card"></div>
-      </div>
-      ;
-      <div className="header">
-        <img src={logo}></img>
-      </div>
+    {!isLoading && 
+        <>
+        <div className="swipe-wrapper">
+          <div className="swipe-card">
+            {
+              <SwipeCard
+              name= {users[userIndex].name}
+              birthDate={users[userIndex].birthDate}
+              photos={
+               users[userIndex].photos
+              }
+              gender= {users[userIndex].gender}
+              biography={users[userIndex].biography}
+              interests={users[userIndex].interests}
+              showNext={showNext}
+            />
+            }
+          </div>
+        </div>
+        <div className="header">
+          <img src={logo}></img>
+        </div>
+        </>
+    }
     </>
   );
 };
--- Ctrl-Love/src/components/SwipeCard.tsx
+++ Ctrl-Love/src/components/SwipeCard.tsx
@@ -3,46 +3,85 @@ import "./SwipeCard.css";
 
 interface SwipeCardProp {
   name: string;
-  age: number;
-  images: string[];
+  birthDate: string;
+  photos: string[];
   gender: string;
   biography: string;
   interests: string[];
   showNext: () => void;
 }
 
+const genders = {
+  0: "Male",
+  1: "Female",
+  2: "Apache Helicopter",
+  3: "Other",
+};
+
 function SwipeCard({
   name,
-  age,
-  images,
+  birthDate,
+  photos,
   gender,
   biography,
   interests,
-  showNext
+  showNext,
 }: SwipeCardProp) {
   const [imageCounter, setImageCounter] = useState(0);
 
   return (
-    <div>
-    <div className="card-container">
-      <div className="top-section">
-        {<img src={images[imageCounter]} alt="profile-pic" className="profile-pic" />}
-      </div>
-      <div className="bottom-section">
-        <h1>
-          {name} : {age}
+    <div className="very-wrapper">
+    <div className="outer-div">
+      <div className="card-container">
+        <div className="pic-div">
+          {
+            <img
+              src={photos[imageCounter]}
+              alt="profile-pic"
+              className="profile-pic"
+            />
+          }
+
+        </div>
+        <h1 className="name-birthDate">
+          {name} -{" "}
+          {new Date().getFullYear() - new Date(birthDate).getFullYear()}
         </h1>
-        <h2>{gender}</h2>
+        <h2>{genders[gender]}</h2>
         <p>{biography}</p>
-        {interests.map((i) => (
-          <p key={i}>{i}</p>
-        ))}
-      </div>
-      <button className="next-pic" onClick={() => {setImageCounter(((imageCounter + 1) % images.length));console.log(imageCounter)}}>→</button>
-      <button className="prev-pic" onClick={() => {setImageCounter(((imageCounter - 1) % images.length)); console.log(imageCounter)}} >←</button>
-      <button className="like-profile" onClick={showNext}>👍</button>
-      <button className="dislike-profile" onClick={showNext}>👎</button>
+        <div className="interests">
+          {interests.map((i) => (
+            <p key={i}>{i}</p>
+          ))}
+        </div>
     </div>
+    <div className="right-button"
+            onClick={() => {
+              setImageCounter((imageCounter + 1) % photos.length);
+            }}
+          >
+            <a href="#">
+              <span className="right"></span>
+            </a>
+          </div>
+
+    <div className="left-button"  onClick={() => {
+              setImageCounter((imageCounter - 1) % photos.length);
+            }}>
+              <a href="#">
+                <span className="left"></span>
+              </a>
+            </div>
+      </div>
+
+      <div className="like-dislike">
+      <button className="dislike-profile" onClick={showNext}>
+        👎
+      </button>
+    <button className="like-profile" onClick={showNext}>
+        👍
+      </button>
+      </div>
     </div>
   );
 }
