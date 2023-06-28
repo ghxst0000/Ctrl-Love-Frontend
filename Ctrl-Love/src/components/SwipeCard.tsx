59ff62ea4ffba6c8b170d025ef1194a83166bb30
dudasdominik <dudasdominik3@gmail.com>
2023-06-28 14:48:53 +0200

    update swipeCard.tsx and css

--- Ctrl-Love/src/components/SwipeCard.tsx
+++ Ctrl-Love/src/components/SwipeCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./SwipeCard.css";
+import ctrlLove from "../assets/logo.svg";
 
 interface SwipeCardProp {
   name: string;
@@ -27,60 +28,108 @@ function SwipeCard({
   interests,
   showNext,
 }: SwipeCardProp) {
-  const [imageCounter, setImageCounter] = useState(0);
+  const [selectedPicNumber, setSelectedPicNum] = useState(0);
 
   return (
-    <div className="very-wrapper">
-    <div className="outer-div">
-      <div className="card-container">
-        <div className="pic-div">
-          {
-            <img
-              src={photos[imageCounter]}
-              alt="profile-pic"
-              className="profile-pic"
-            />
-          }
-
+    <div className="swipe-card-outer-wrapper">
+      <div className="content">
+        <div className="swipe-card-upper-section-wrapper">
+          <div
+            className="swipe-card-backward-picture"
+            onClick={() =>
+              setSelectedPicNum(
+                (selectedPicNumber + photos.length - 1) % photos.length
+              )
+            }
+          >
+            <span>{"<"}</span>
+          </div>
+          <div className="box_parent">
+            <div
+              className="box2"
+              style={{ backgroundImage: `url(${photos[selectedPicNumber]})` }}
+            ></div>
+            <svg className="flt_svg" xmlns="http://www.w3.org/2000/svg%22%3E">
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="
+                  1 0 0 0 0 
+                  0 1 0 0 0 
+                  0 0 1 0 0 
+                  0 1 0 19 -10"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+          <div className="rectangle-33"></div>
+          <div
+            className="swipe-card-forward-picture"
+            onClick={() =>
+              setSelectedPicNum((selectedPicNumber + 1) % photos.length)
+            }
+          >
+            <span>{">"}</span>
+          </div>
         </div>
-        <h1 className="name-birthDate">
-          {name} -{" "}
-          {new Date().getFullYear() - new Date(birthDate).getFullYear()}
-        </h1>
-        <h2>{genders[gender]}</h2>
-        <p>{biography}</p>
-        <div className="interests">
-          {interests.map((i) => (
-            <p key={i}>{i}</p>
+        <div className="bubbles-for-picture-count">
+          {photos.map((p, index) => (
+            <div
+              key={index}
+              style={
+                selectedPicNumber === index
+                  ? { backgroundColor: "#7cf4d2" }
+                  : { backgroundColor: "#fde8ee" }
+              }
+              className="bubble"
+            ></div>
           ))}
         </div>
-    </div>
-    <div className="right-button"
-            onClick={() => {
-              setImageCounter((imageCounter + 1) % photos.length);
-            }}
-          >
-            <a href="#">
-              <span className="right"></span>
-            </a>
+        <div className="information-about-user">
+          <b>
+            <span>{name}</span>
+          </b>
+          <b>
+            <span>{gender}</span>
+          </b>
+        </div>
+        <div className="information-age-wrapper">
+          <div className="information-age">
+            <b>
+              <span>{birthDate}</span>
+            </b>
           </div>
-
-    <div className="left-button"  onClick={() => {
-              setImageCounter((imageCounter - 1) % photos.length);
-            }}>
-              <a href="#">
-                <span className="left"></span>
-              </a>
+        </div>
+        <div className="biography-content">{biography}</div>
+        <div className="interest-content">
+          {interests.map((i, index) => (
+            <div key={index} className="interest-bubble">
+              {i}
             </div>
-      </div>
-
-      <div className="like-dislike">
-      <button className="dislike-profile" onClick={showNext}>
-        👎
-      </button>
-    <button className="like-profile" onClick={showNext}>
-        👍
-      </button>
+          ))}
+        </div>
+        <div className="button-container">
+          <button className="no" onClick={showNext}>
+            No
+          </button>
+          <button className="yes" onClick={showNext}>
+            Yes
+          </button>
+        </div>
       </div>
     </div>
   );
