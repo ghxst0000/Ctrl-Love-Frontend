d3617e63c8d3e9124eb37608930b755f5f6f4577
Marci Kacsan <marcikacsan@pop-os.localdomain>
2023-07-26 15:01:24 +0200

    fixfor build

--- Ctrl-Love/src/components/MyProfile.tsx
+++ Ctrl-Love/src/components/MyProfile.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./MyProfile.css";
-import logo from "../assets/logo.svg";
 
 interface MyProfileProp {
     id: string;
